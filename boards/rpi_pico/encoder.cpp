4f2e400c78145a420114257df3333198aa355326
Olawale Peace <olawalepeace8@gmail.com>
2026-08-25 00:06:47 +0100

    Refactor Encoder class: ISR isnow very minimal, no floating point arithmetic

--- boards/rpi_pico/encoder.cpp
+++ boards/rpi_pico/encoder.cpp
@@ -12,58 +12,46 @@ Encoder::~Encoder() {
 
 void Encoder::initialize(uint32_t timer_window_ms, uint8_t pulse_window_intervals) {
     // Initialization code for the encoder
-    this->timer_window_ms = timer_window_ms;
-    this->pulse_window_intervals = pulse_window_intervals;
+    timer_window_.time_interval_us = timer_window_ms*1000;
+    pulse_window_.pulse_count = pulse_window_intervals;
     // Here, I will create the two interrupts
     reset();
 }
 
 void Encoder::pulse_counter_callback() {
     // Callback function to handle pulse counting
-    pulse_counter.pulse_count++;
-    pulse_counter.timestamp_us = to_us_since_boot(get_absolute_time());
-    if (pulse_counter.pulse_count == 1) {
-        last_pulse_window_timestamp_us = pulse_counter.timestamp_us;
+    pulse_counter_.pulse_count++;
+    pulse_counter_.timestamp_us = to_us_since_boot(get_absolute_time());
+    if (pulse_counter_.pulse_count == 1) {
+        last_pulse_window_timestamp_us_ = pulse_counter_.timestamp_us;
         return; // Skip speed calculation for the first pulse, if not, it'll calculate a speed of 0, harmless but not necessary
     }
-    if ((pulse_counter.pulse_count-1) % pulse_window_intervals == 0) { //subtracting 1 because we start counting from 1
-        pulse_window_angular_speed = determine_pulse_window_angular_speed(pulse_counter.timestamp_us);
+    if ((pulse_counter_.pulse_count-1) % pulse_window_.pulse_count == 0) { //subtracting 1 because we start counting from 1
+        pulse_window_.time_interval_us = pulse_counter_.timestamp_us - last_pulse_window_timestamp_us_;
+        last_pulse_window_timestamp_us_ = pulse_counter_.timestamp_us;
     }
 }
 
 void Encoder::encoder_timer_window_callback() {
     // Callback function to handle timer window events
-    time_window_angular_speed = determine_time_window_angular_speed();
+    timer_window_.pulse_count = pulse_counter_.pulse_count - last_timer_window_pulse_count_;
+    last_timer_window_pulse_count_ = pulse_counter_.pulse_count; 
 }
 
 void Encoder::reset() {
-    pulse_counter.pulse_count = 0;
-    pulse_counter.timestamp_us = 0;
-    last_timer_window_pulse_count = 0;
-    last_pulse_window_timestamp_us = 0;
+    pulse_counter_.pulse_count = 0;
+    pulse_counter_.timestamp_us = 0;
+    last_timer_window_pulse_count_ = 0;
+    last_pulse_window_timestamp_us_ = 0;
 }
 
-float Encoder::determine_time_window_angular_speed() {
-    // Implementation for determining angular speed with time window
-    uint32_t pulse_count_diff = pulse_counter.pulse_count - last_timer_window_pulse_count;
-    last_timer_window_pulse_count = pulse_counter.pulse_count;    
-    return determine_angular_speed(pulse_count_diff, timer_window_ms * 1000); // Convert ms to us   
-}
-
-float Encoder::determine_pulse_window_angular_speed(uint64_t timestamp_us) {
-    // Implementation for determining angular speed with pulse window
-    uint64_t time_diff_us = (timestamp_us - last_pulse_window_timestamp_us);
-    last_pulse_window_timestamp_us = timestamp_us;
-    return determine_angular_speed(pulse_window_intervals, time_diff_us);
-}
-
-float Encoder::determine_angular_speed(uint64_t pulse_count_diff, uint64_t time_diff_us) {
+float Encoder::determine_angular_speed(const Encoder::PulseWindow& pulse_window, uint8_t ppr) {
     // Calculate angular speed based on pulse count difference and time difference
-    if (time_diff_us == 0) {
+    if (pulse_window.time_interval_us == 0 or ppr == 0) {
         return 0.0f; // Avoid division by zero
     }
-    float revolutions = (float)pulse_count_diff / ppr_;
-    float time_diff_s = (float)time_diff_us / 1000000.0f; // Convert microseconds to seconds
+    float revolutions = (float)pulse_window.pulse_count / ppr; // Calculate revolutions based on pulse count and pulses per revolution
+    float time_diff_s = (float)pulse_window.time_interval_us / 1000000.0f; // Convert microseconds to seconds
     float angular_speed = revolutions / time_diff_s; // in revolutions per second
     return angular_speed * 2.0f * 3.14159265358979323846f; // Convert to radians per second
 }
--- include/encoder.h
+++ include/encoder.h
@@ -11,25 +11,27 @@ class Encoder {
         void encoder_timer_window_callback();
         float determine_time_window_angular_speed();
         float determine_pulse_window_angular_speed(uint64_t timestamp_us);
-        // float get_time_window_angular_speed() { return time_window_angular_speed; }
-        // float get_pulse_window_angular_speed() { return pulse_window_angular_speed; }
         void reset();
 
     private:
-        struct pulse_counter {
-            int32_t pulse_count;
-            uint64_t timestamp_us;
-        }pulse_counter;
+        struct PulseCounter {
+            volatile int32_t pulse_count;
+            volatile uint64_t timestamp_us;
+        }pulse_counter_;
+
+        struct PulseWindow{
+            volatile uint32_t pulse_count;
+            volatile uint64_t time_interval_us;
+        };
+        
+        PulseWindow pulse_window_; // Determine angular speed based on elapsed time between set pulse window
+        PulseWindow timer_window_; // Determine angular speed based on pulse count within a time window
 
         uint8_t gpio_, ppr_;
 
-        uint8_t pulse_window_intervals;
-        uint32_t timer_window_ms;
-        float pulse_window_angular_speed, time_window_angular_speed;
-
-        uint32_t last_timer_window_pulse_count;
-        uint64_t last_pulse_window_timestamp_us;
-        float determine_angular_speed(uint64_t pulse_count_diff, uint64_t time_diff_us);
+        uint32_t last_timer_window_pulse_count_;
+        uint64_t last_pulse_window_timestamp_us_;
+        static float determine_angular_speed(const PulseWindow& pulse_window, uint8_t ppr);
 };
 
 #endif // POLOTICUS_ENCODER_H
